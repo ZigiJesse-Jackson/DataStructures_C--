17225339f80e55d16ea5e44eadb935df77e02ecc
Jesse-Jackson Zigi <59916143+ZigiJesse-Jackson@users.noreply.github.com>
2022-06-16 04:58:28 +0000

    complete generic BinaryTree with inorder printing

---
+++ List-Type_Data_Structures/BinaryTree.cpp
@@ -0,0 +1,139 @@
+#include "BinaryNode.cpp"
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+template<typename T>
+class BinaryTree{
+    BinaryNode<T>* root; 
+
+    // Helper function for BinaryTree destructor
+    void deleteBinaryTree(BinaryNode<T>* node){
+            if(node==nullptr){
+                return;
+            }
+            deleteBinaryTree(node->left);
+            deleteBinaryTree(node->right);
+            delete node;
+        }
+
+    // Prints tree inorder fashion     
+    void printTree(BinaryNode<T>* node){
+        // node with no children
+        if(node->left==nullptr && node->right==nullptr){
+            std::cout << node->data;
+            return;
+        }
+
+        // initializing current node as root
+        BinaryNode<T>* curr = node;
+        std::vector<BinaryNode<T>*> stack;
+        // traversing inorder iterative
+        while (curr!=nullptr || stack.empty()==false){
+            // using stack to keep track of Depth
+            while(curr!=nullptr){
+                stack.push_back(curr);
+                curr = curr->left;
+            }
+
+            // printing tree inorder
+            curr = stack.back();
+            std::cout << curr->data << " ";
+            stack.pop_back();
+            curr = curr->right;
+        }
+    }
+
+    public:
+        // 0-paramter constructor
+        BinaryTree(){
+            root = new BinaryNode<T>();
+        }
+
+        // parameter constructor
+        BinaryTree(BinaryNode<T>* node){
+            root = node;
+        }
+
+        // copy constructor
+        BinaryTree(BinaryTree & tree){
+            root = new BinaryNode<T>(tree.root->data);
+        }
+
+        // move constructor 
+        BinaryTree(BinaryTree && tree):root{std::move(tree.root)}{
+            tree.root = nullptr;
+        }
+
+        // copy assignment
+        BinaryTree& operator=(BinaryTree& other){
+            root = other.root;
+        }
+
+        // move assignment
+        BinaryTree& operator=(BinaryTree&& other){
+            std::swap(root, other.root);
+            other.root = nullptr;
+            
+        }
+
+        void insert(BinaryNode<T>* inserted_node){
+
+            // Tree is empty
+            if(root == nullptr)return;
+
+            // Node to be inserted has the same key as root
+            if(root->key == inserted_node->key){
+                inserted_node->left = root->left;
+                inserted_node->right = root->right;
+                std::swap(inserted_node,root);
+                delete inserted_node;
+                return;
+            }
+            BinaryNode<T>* parent = root;
+            bool notFound = true;
+            while(notFound){
+                // Node to be inserted should be on right side of parent
+                if(parent->key>inserted_node->key){
+                    // Spot found for node to be inserted as leaf
+                    if(parent->right==nullptr){
+                        parent->right = inserted_node;
+                        break;
+                    }
+                    parent = parent->right;
+                    continue;
+                    }
+                // Node to be inserted should be on left side of parent
+                if(parent->key<inserted_node->key){
+                    // Spot found for node to be inserted as leaf
+                    if(parent->left==nullptr){
+                        parent->left = inserted_node;
+                        break;
+                    }
+                    parent = parent->left;
+                    continue;
+                }
+                // Node to be inserted should be inserted as parent
+                if(parent->key==inserted_node->key){
+                    inserted_node->left = parent->left;
+                    inserted_node->right = parent->right;
+                    std::swap(parent->data, inserted_node->data);
+                    delete inserted_node;
+                    break;
+                }
+            }
+            return;    
+        }
+
+        // print content of tree inorder
+        void printTree(){
+            printTree(root);
+        }
+
+        // BinaryTree destructor
+        ~BinaryTree(){
+            deleteBinaryTree(root);
+            return;
+        }
+};
+
--- main.cpp
+++ main.cpp
@@ -1,13 +1,43 @@
 #include <iostream>
-#include <string>
-#include "TechnicalInterviewQuestions/GoogleQuestions/ShortestToFormString.cpp"
-#include <array>
+#include "./List-Type_Data_Structures/BinaryTree.cpp"
 
 int main()
 {   
-    std::string src = "xyxz";
-    std::string target = "xzyxxz";
-    std::cout << short_to_form_string(src, target);
+    BinaryNode<int>* root = new BinaryNode<int>(8, 8);
+    BinaryTree<int> tree = BinaryTree<int>(root);
+    BinaryNode<int>* n_4 = new BinaryNode<int>(4,4);
+    BinaryNode<int>* n_12 = new BinaryNode<int>(12,12);
+    BinaryNode<int>* n_2 = new BinaryNode<int>(2,2);
+    BinaryNode<int>* n_6 = new BinaryNode<int>(6,6);
+    BinaryNode<int>* n_10 = new BinaryNode<int>(10,10);
+    BinaryNode<int>* n_14 = new BinaryNode<int>(14,14);
+    BinaryNode<int>* n_1 = new BinaryNode<int>(1,1);
+    BinaryNode<int>* n_3 = new BinaryNode<int>(3,3);
+    BinaryNode<int>* n_5 = new BinaryNode<int>(5,5);
+    BinaryNode<int>* n_7 = new BinaryNode<int>(7,7);
+    BinaryNode<int>* n_9 = new BinaryNode<int>(9,9);
+    BinaryNode<int>* n_11 = new BinaryNode<int>(11,11);
+    BinaryNode<int>* n_13 = new BinaryNode<int>(13,13);
+    BinaryNode<int>* n_15 = new BinaryNode<int>(15,15);
+    BinaryNode<int>* n_17 = new BinaryNode<int>(94,29);
+
+    tree.insert(n_4);
+    tree.insert(n_12);
+    tree.insert(n_2);
+    tree.insert(n_6);
+    tree.insert(n_10);
+    tree.insert(n_14);
+    tree.insert(n_1);
+    tree.insert(n_3);
+    tree.insert(n_5);
+    tree.insert(n_7);
+    tree.insert(n_9);
+    tree.insert(n_11);
+    tree.insert(n_13);
+    tree.insert(n_15);
+    tree.insert(n_17);  
+
+    tree.printTree();
     
     return 0;
 }
